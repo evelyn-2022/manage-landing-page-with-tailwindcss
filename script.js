7fd89100a5fabab6e8d7741ad40b3e4d40f5f215
evelyn-2022 <evelyn20220101@gmail.com>
2022-06-27 22:50:19 +0800

    added overlay

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-container');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('open');
   menu.classList.toggle('open');
+  overlay.classList.toggle('hidden');
+  body.classList.toggle('overflow-y-hidden');
 });
