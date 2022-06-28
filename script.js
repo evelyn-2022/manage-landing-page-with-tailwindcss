51e9afa1447bf2b31133d80ea511850833955031
evelyn-2022 <evelyn20220101@gmail.com>
2022-06-28 16:07:09 +0800

    finished slider part

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Nav Menu
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-container');
 const overlay = document.querySelector('.overlay');
@@ -7,4 +8,30 @@ hamburger.addEventListener('click', function () {
   menu.classList.toggle('open');
   overlay.classList.toggle('hidden');
   body.classList.toggle('overflow-y-hidden');
+});
+
+// Testimonials
+const controlContainer = document.querySelector('.controls');
+const controlBtns = document.querySelectorAll('.control-btn');
+const slides = document.querySelectorAll('.slide');
+controlContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.control-btn');
+
+  if (!clicked) return;
+
+  const num = clicked.dataset.control;
+  controlBtns.forEach(btn => {
+    btn.classList.remove('control-active');
+  });
+  clicked.classList.add('control-active');
+
+  slides.forEach(slide => {
+    if (slide.classList.contains(`testi-${num}`)) {
+      slide.classList.remove('hidden');
+      slide.classList.add('flex');
+    } else {
+      slide.classList.add('hidden');
+      slide.classList.remove('flex');
+    }
+  });
 });
